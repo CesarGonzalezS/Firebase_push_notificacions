c71a4716334834f203b740420a4bb3b20dad64fb
Cesar Gonzalez S. <94254731+CesarGonzalezS@users.noreply.github.com>
2024-11-14 11:20:30 -0600

    [UPDATE]Se agrego la configuracion de firebase

--- lib/main.dart
+++ lib/main.dart
@@ -5,144 +5,128 @@ import 'firebase_options.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
-/// Manejador para recibir mensajes de Firebase Cloud Messaging (FCM).
-/// 
-/// Esta función asíncrona se invoca al recibir un mensaje de FCM.
-/// Inicializa Firebase y luego imprime el título del mensaje recibido en la consola.
-Future<void> _firebaseMessangingHandler(RemoteMessage message) async {
+/// Función para manejar mensajes en segundo plano de Firebase Messaging.
+/// Inicializa Firebase y muestra el título del mensaje recibido en la consola.
+Future<void> _firebaseMessagingHandler(RemoteMessage message) async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  print("Estoy capturando el mensaje: ${message.notification!.title}");
+  print(
+      "Estoy capturando el mensaje en segundo plano: ${message.notification?.title}");
 }
 
-/// Configuración específica de notificaciones para Android.
-/// 
-/// Utiliza el icono predeterminado de la app para mostrar las notificaciones en dispositivos Android.
+/// Configuración específica para Android.
+/// Utiliza el icono de la aplicación como icono de notificación en Android.
 const AndroidInitializationSettings initializationSettingsAndroid =
-    AndroidInitializationSettings('@mipmap/ic_launch');
+    AndroidInitializationSettings('@mipmap/ic_launcher');
 
-/// Configuración específica de notificaciones para iOS (Darwin).
+/// Configuración específica para iOS (usando la configuración de Darwin).
+/// Esta configuración permite personalizar las notificaciones en dispositivos Apple.
 final DarwinInitializationSettings initializationSettingsDarwin =
     DarwinInitializationSettings();
 
-/// Configuración general de inicialización de notificaciones.
-/// 
-/// Incluye las configuraciones para Android e iOS, necesarias para mostrar notificaciones en ambas plataformas.
+/// Configuración general de inicialización para notificaciones locales.
+/// Combina las configuraciones específicas de Android e iOS.
 final InitializationSettings initializationSettings = InitializationSettings(
   android: initializationSettingsAndroid,
   iOS: initializationSettingsDarwin,
 );
 
-/// Punto de entrada principal de la aplicación.
-/// 
-/// Se asegura de inicializar los Widgets de Flutter, inicializa Firebase y luego ejecuta la aplicación.
+/// Instancia del plugin de notificaciones locales.
+/// Esta instancia permite mostrar notificaciones dentro de la aplicación.
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // Configura un controlador para manejar mensajes de Firebase en segundo plano.
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingHandler);
+
+  // Inicializa el plugin de notificaciones locales.
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+  // Ejecuta la aplicación principal.
   runApp(const MyApp());
 }
 
-/// Clase principal de la aplicación.
-/// 
-/// Define la estructura básica de la interfaz de usuario, incluyendo el tema y la página principal.
-class MyApp extends StatelessWidget {
+/// Clase principal de la aplicación que representa el widget raíz.
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  /// Construye y retorna el widget raíz de la aplicación.
-  /// 
-  /// Configura el título y tema de la aplicación, y define la página principal.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 126, 28, 134),
-        ),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  State<MyApp> createState() => _MyAppState();
+}
+
+/// Estado de la clase `MyApp` donde se configura el permiso para notificaciones.
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Instancia de Firebase Messaging para gestionar notificaciones.
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+    // Solicita permisos para recibir notificaciones en el dispositivo.
+    messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: false,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
     );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+    // Configura un controlador para manejar mensajes recibidos mientras la app está en primer plano.
+    FirebaseMessaging.onMessage.listen((RemoteMessage remoteMessage) {
+      print("Estoy obteniendo una notificación en primer plano");
+      print("Datos de la notificación: ${remoteMessage.data}");
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+      // Muestra una notificación local cuando se recibe un mensaje en primer plano.
+      flutterLocalNotificationsPlugin.show(
+        remoteMessage.hashCode,
+        remoteMessage.notification?.title,
+        remoteMessage.notification?.body,
+        const NotificationDetails(
+          android: AndroidNotificationDetails(
+            'id_de_prueba',
+            'Canal_de_prueba',
+            channelDescription: 'Demostración',
+            importance: Importance.max,
+            priority: Priority.max,
+          ),
+        ),
+      );
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+      if (remoteMessage.notification != null) {
+        print("Mensaje: ${remoteMessage.notification}");
+      }
+    });
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+    // Obtiene y muestra el token de dispositivo.
+    FirebaseMessaging.instance.getToken().then((token) {
+      print("Token del dispositivo: $token");
     });
   }
 
+  //dlG3m4o2Qja5V9GeNQFukd:APA91bGQ3xrAWmu9ny3vDHnhkRwnLxcMeA-wpuG6j3p7a5XjNVTOH_Qg6I252Vjv4Zxszw9ApCVgAu4wAo99Y81uiv5hktk4oFhGQGOo-NTIitcIfCRQMJo
+
+  /// Construye el widget principal de la aplicación.
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Notificaciones push"),
+        ),
+        body: const Center(
+          child: Text("ESPERA DE LA NOTIFICACION"),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
